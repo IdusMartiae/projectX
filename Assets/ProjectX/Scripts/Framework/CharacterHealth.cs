537295cb06224afce33083983a6e9c4a32fd7b4b
Anton Zhylko <antonzhylko@gmail.com>
2021-08-02 17:36:08 +0300

    Add methods for healing/damage dealing

--- Assets/ProjectX/Scripts/Framework/CharacterHealth.cs
+++ Assets/ProjectX/Scripts/Framework/CharacterHealth.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace ProjectX.Scripts.Framework
 {
+    // TODO method basically have the same code, merge where possible
     public class CharacterHealth : MonoBehaviour
     {
         [SerializeField] private float maxHealth;
@@ -9,8 +10,10 @@ namespace ProjectX.Scripts.Framework
         private float _currentHealth;
 
         // TODO REMOVE AFTER DEBUGGING
+        // ----------------------------
         public float CurrentHealth => _currentHealth;
         public float MaxHealth => maxHealth;
+        // ----------------------------
         
         public void Start()
         {
@@ -20,13 +23,35 @@ namespace ProjectX.Scripts.Framework
         public void TakeDamage(float damage)
         {
             _currentHealth = Mathf.Max(_currentHealth - damage, 0);
-            Debug.Log($"{gameObject.name}'s health is {_currentHealth} points");
-            if (_currentHealth == 0)
-            {
-                Debug.Log($"{gameObject.name} died.");
-            } 
+        }
+
+        public void TakeDamagePercentOfMax(float healthPercent)
+        {
+            healthPercent = Mathf.Clamp01(healthPercent);
+            _currentHealth = Mathf.Max(_currentHealth - maxHealth * healthPercent, 0);
+        }
+
+        public void TakeDamagePercentOfCurrent(float healthPercent)
+        {
+            healthPercent = Mathf.Clamp01(healthPercent);
+            _currentHealth = Mathf.Max(_currentHealth - _currentHealth * healthPercent, 0);
         }
         
-        
+        public void Regenerate(float healthRecovered)
+        {
+            _currentHealth = Mathf.Min(_currentHealth + healthRecovered, maxHealth);
+        }
+
+        public void RegeneratePercentOfMax(float healthRecoveredPercent)
+        {
+            healthRecoveredPercent = Mathf.Clamp01(healthRecoveredPercent);
+            _currentHealth = Mathf.Min(_currentHealth + maxHealth * healthRecoveredPercent, maxHealth);
+        }
+
+        public void RegeneratePercentOfCurrent(float healthRecoveredPercent)
+        {
+            healthRecoveredPercent = Mathf.Clamp01(healthRecoveredPercent);
+            _currentHealth = Mathf.Min(_currentHealth + _currentHealth * healthRecoveredPercent, maxHealth);
+        }
     }
 }

326b7e066951a45e1b75d595d70e0ef2f798f0a3
Anton Zhylko <39462249+IdusMartiae@users.noreply.github.com>
2021-08-18 12:02:10 +0300

    Add self targeting strategy

---
+++ Assets/ProjectX/Scripts/Framework/Abilities/Targeting/SelfTargeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectX.Scripts.Framework.Abilities.Targeting
+{
+    [CreateAssetMenu(fileName = "targeting_self", menuName = "Abilities/Targeting/Self")]
+    public class SelfTargeting : TargetingStrategy
+    {
+        public override void AcquireTargets(AbilityData data, Action callback)
+        {
+            data.Targets = new[] {data.User};
+            callback();
+        }
+    }
+}
